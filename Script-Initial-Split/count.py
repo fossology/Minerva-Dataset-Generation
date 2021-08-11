1e8f6e7feeda3e3636d7f7f1df232e312e6a76e9
Shreya <shreya.out@gmail.com>
2021-08-11 15:54:42 +0530

    Added files

---
+++ Script-Initial-Split/count.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import argparse
+
+def main(path):
+    file = []
+    count = []
+
+    for filename in os.listdir(path):
+        file.append(filename)
+        lst = os.listdir(os.path.join(path,filename)) # dir is your directory path
+        number_files = len(lst)
+        count.append(number_files)           
+
+    data = pd.DataFrame({"files":file,"count":count})
+    data.to_csv("Count.csv", index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='Pass a directory to find original licenses')
+    args = parser.parse_args()
+    path = args.path
+    main(path)
